caa5a0cac2350e4db7d405e24ee8a4a248d28c7e
jasonmcauley <jasonmcauley@gmail.com>
2013-04-13 09:15:53 -0400

    First Commit of helloworld

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,10 @@
+class SayController < ApplicationController
+  def hello
+    @time = Time.now
+    @files = Dir.glob('*')
+  end
+
+  def goodbye
+    @time = Time.now.end_of_week
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'b8e104b71a6330fda7d453afe5fca395c7c1d29aa510a27a455b2bf8ea2564bb8e58ac53e5c9b42db1e2f824ceabd62151351ce31d351cd53b52c766b04ea40b'
